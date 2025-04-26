59d0f4f43a60e4af186bcae73b0ca6c561625098
Николай Притыкин <nikolajpritykin@MacBook-Air-Nikolaj.local>
2025-04-26 17:56:23 +0700

    change funcs

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -26,98 +26,20 @@ class UsersController extends Controller
             'education'=> 'string',
             'is_accept_data'=> 'string',
         ]);
-        $score = 0;
-        //Считаем скоринг по первым 3 цифрам номера телефона
-        $megaphone_nums = array('920', '921', '922', '923', '924', '925', '926', '927', '928', '929');
-        $bilain_nums = array('903','905','906','909','960','961','962','963','964','965','967');
-        $mts_nums = array('910','915','916','917','911','981','918','988','989','912','917','919','982','983','914','984');
 
-        $num_first = substr($user['phone'],0,1);
-        if ($num_first == "+") {
-            $num_pref = substr($user["phone"],2,3);
-            if (in_array($num_pref, $megaphone_nums)) {
-                $score += 10;
-            } elseif (in_array($num_pref, $bilain_nums)) {
-                $score += 5;
-            } elseif (in_array($num_pref, $mts_nums)) {
-                $score += 3;
-            } else {
-                $score += 1;
-            }
-        } elseif($num_first == "7") {
-            $num_pref = substr($user["phone"],1,3);
-            if (in_array($num_pref, $megaphone_nums)) {
-                $score += 10;
-            } elseif (in_array($num_pref, $bilain_nums)) {
-                $score += 5;
-            } elseif (in_array($num_pref, $mts_nums)) {
-                $score += 3;
-            } else {
-                $score += 1;
-            }
-        } elseif ($num_first == "9") {
-            $nums_pref = substr($user["phone"],0,3);
-            if (in_array($nums_pref, $megaphone_nums)) {
-                $score += 10;
-            } elseif (in_array($nums_pref, $bilain_nums)) {
-                $score += 5;
-            } elseif (in_array($nums_pref, $mts_nums)) {
-                $score += 3;
-            } else {
-                $score += 1;
-            }
-        } else {
-            $user["phone"] = 'not russian phone numbers';
-            $score += 0;
-        }
+        //Считаем скоринг через функцию
+        $score = User::scoring($user);
 
-        //Считаем скоринг по домену почты
-        $email_explode = explode("@", $user["email"]);
-        $email_user = explode(".", $email_explode[1]);
-        if ($email_user[0] == "gmail") {
-            $score += 10;
-        } elseif ($email_user[0] == "yandex") {
-            $score += 8;
-        } elseif ($email_user[0] == "mail") {
-            $score += 6;
-        } else {
-            $score += 3;
-        }
+        User::create([
+            "first_name"=> $user["first_name"],
+            "last_name"=> $user["last_name"],
+            "phone"=> $user["phone"],
+            "email"=> $user["email"],
+            "education"=> $user["education"],
+            "is_accept_data"=> $user["is_accept_data"],
+            "scoring"=> $score,
+        ]);
 
-        //Считаем скоринг по образованию
-        $edu = $user["education"];
-        if ($edu == "Высшее образование") {
-            $score += 15;
-        } elseif ($edu == "Специальное образование") {
-            $score += 10;
-        } else {
-            $score += 5;
-        }
-
-        //Считаем скоринг по галочке
-        if (isset($user["is_accept_data"])) {
-            $score += 4;
-            User::create([
-                "first_name"=> $user["first_name"],
-                "last_name"=> $user["last_name"],
-                "phone"=> $user["phone"],
-                "email"=> $user["email"],
-                "education"=> $user["education"],
-                "is_accept_data"=> $user["is_accept_data"],
-                "scoring"=> $score,
-            ]);
-        } else {
-            $score += 0;
-            User::create([
-                "first_name"=> $user["first_name"],
-                "last_name"=> $user["last_name"],
-                "phone"=> $user["phone"],
-                "email"=> $user["email"],
-                "education"=> $user["education"],
-                "is_accept_data"=> 'false',
-                "scoring"=> $score,
-            ]);
-        }
         return redirect()->route('users.index');
     }
 
@@ -139,98 +61,18 @@ class UsersController extends Controller
             'is_accept_data'=> 'string',
         ]);
 
-        $score = 0;
-        //Считаем скоринг по первым 3 цифрам номера телефона
-        $megaphone_nums = array('920', '921', '922', '923', '924', '925', '926', '927', '928', '929');
-        $bilain_nums = array('903','905','906','909','960','961','962','963','964','965','967');
-        $mts_nums = array('910','915','916','917','911','981','918','988','989','912','917','919','982','983','914','984');
+        //Считаем скоринг через функцию
+        $score = User::scoring($user_update);
 
-        $num_first = substr($user_update['phone'],0,1);
-        if ($num_first == "+") {
-            $num_pref = substr($user_update["phone"],2,3);
-            if (in_array($num_pref, $megaphone_nums)) {
-                $score += 10;
-            } elseif (in_array($num_pref, $bilain_nums)) {
-                $score += 5;
-            } elseif (in_array($num_pref, $mts_nums)) {
-                $score += 3;
-            } else {
-                $score += 1;
-            }
-        } elseif($num_first == "7") {
-            $num_pref = substr($user_update["phone"],1,3);
-            if (in_array($num_pref, $megaphone_nums)) {
-                $score += 10;
-            } elseif (in_array($num_pref, $bilain_nums)) {
-                $score += 5;
-            } elseif (in_array($num_pref, $mts_nums)) {
-                $score += 3;
-            } else {
-                $score += 1;
-            }
-        } elseif ($num_first == "9") {
-            $nums_pref = substr($user_update["phone"],0,3);
-            if (in_array($nums_pref, $megaphone_nums)) {
-                $score += 10;
-            } elseif (in_array($nums_pref, $bilain_nums)) {
-                $score += 5;
-            } elseif (in_array($nums_pref, $mts_nums)) {
-                $score += 3;
-            } else {
-                $score += 1;
-            }
-        } else {
-            $user_update["phone"] = 'not russian phone numbers';
-            $score += 0;
-        }
-
-        //Считаем скоринг по домену почты
-        $email_explode = explode("@", $user_update["email"]);
-        $email_user = explode(".", $email_explode[1]);
-        if ($email_user[0] == "gmail") {
-            $score += 10;
-        } elseif ($email_user[0] == "yandex") {
-            $score += 8;
-        } elseif ($email_user[0] == "mail") {
-            $score += 6;
-        } else {
-            $score += 3;
-        }
-
-        //Считаем скоринг по образованию
-        $edu = $user_update["education"];
-        if ($edu == "Высшее образование") {
-            $score += 15;
-        } elseif ($edu == "Специальное образование") {
-            $score += 10;
-        } else {
-            $score += 5;
-        }
-
-        //Считаем скоринг по галочке
-        if (isset($user_update["is_accept_data"])) {
-            $score += 4;
-            $user->update([
-                "first_name"=> $user_update["first_name"],
-                "last_name"=> $user_update["last_name"],
-                "phone"=> $user_update["phone"],
-                "email"=> $user_update["email"],
-                "education"=> $user_update["education"],
-                "is_accept_data"=> $user_update["is_accept_data"],
-                "scoring"=> $score,
-            ]);
-        } else {
-            $score += 0;
-            $user->update([
-                "first_name"=> $user_update["first_name"],
-                "last_name"=> $user_update["last_name"],
-                "phone"=> $user_update["phone"],
-                "email"=> $user_update["email"],
-                "education"=> $user_update["education"],
-                "is_accept_data"=> 'false',
-                "scoring"=> $score,
-            ]);
-        }
+        $user->update([
+            "first_name"=> $user_update["first_name"],
+            "last_name"=> $user_update["last_name"],
+            "phone"=> $user_update["phone"],
+            "email"=> $user_update["email"],
+            "education"=> $user_update["education"],
+            "is_accept_data"=> $user_update["is_accept_data"],
+            "scoring"=> $score,
+        ]);
 
         return redirect()->route('users.show', $user->id);
     }
